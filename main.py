c09be176aed7f400a4fa73b5b63eb8a92a0d4f56
Sank <71709931+Sank34@users.noreply.github.com>
2022-02-19 12:21:50 +0200

    Version 0.0.2

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import qrcode#importing the module
-from PIL import Image
-print("Welcome to the qrCode generator.")
-ls = input(print("Please type here your link:"))#ls will store the link
-img = qrcode.make(ls)#making the qrCode
-img.save("Link.png")#saving the qrCode
-print("thanks for using our generator,please see your project files for the qr code.(Link.png)")#change it if you want
+import qrcode  # importing the module
+try:
+  print("Welcome to the qrCode generator.\nRemember that you can put also text,not just links.")
+  ls = input("Please type here your link or message:")  # var ls will store the link
+  print("Your link/message:" + ls)
+  img = qrcode.make(ls)  # making the qrCode
+  img.save("Link.png")  # saving the qrCode
+  print("thanks for using our generator,please see your project files for the QR code.(Link.png)")  # final message
+# if an error is detected
+except:
+    print("An unknown error ocurred during processing your QRcode,plese contact support.")
+
